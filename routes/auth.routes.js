dcb928230742ee24bd38be4f8bca26974ad2dd7b
Wahid Halim <wahidkeo@gmail.com>
2025-11-27 16:55:23 +0430

    Add subscription routes and auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,34 +1,25 @@
 import { Router } from "express";
 const authRouter = Router();
 
-authRouter.post(
-  ("/sign-up",
-  (req, res) => {
-    res.json({
-      success: true,
-      message: "sign up",
-    });
-  })
-);
+authRouter.post("/sign-up", (req, res) => {
+  res.json({
+    success: true,
+    message: "sign up",
+  });
+});
 
-authRouter.post(
-  ("/sign-in",
-  (req, res) => {
-    res.json({
-      success: true,
-      message: "sign in",
-    });
-  })
-);
+authRouter.post("/sign-in", (req, res) => {
+  res.json({
+    success: true,
+    message: "sign in",
+  });
+});
 
-authRouter.post(
-  ("/sign-out",
-  (req, res) => {
-    res.json({
-      success: true,
-      message: "sign out",
-    });
-  })
-);
+authRouter.post("/sign-out", (req, res) => {
+  res.json({
+    success: true,
+    message: "sign out",
+  });
+});
 
 export default authRouter;
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,7 +1,28 @@
 import { Router } from "express";
 const subscriptionRouter = Router();
 
+subscriptionRouter.get("/", (req, res) => res.send("GET all subscription"));
 
-subscriptionRouter.post('/', (req,res) => res.send(''))
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send("GET subscription details")
+);
+
+subscriptionRouter.post("/", (req, res) => res.send("create subscription"));
+
+subscriptionRouter.put("/:id", (req, res) => res.send("update subscription"));
+
+subscriptionRouter.delete("/", (req, res) => res.send("Delete subscription"));
+
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send("GET all user subscription")
+);
+
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send("cancel subscription")
+);
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) =>
+  res.send("get upcoming renewals subscription")
+);
 
 export default subscriptionRouter;
