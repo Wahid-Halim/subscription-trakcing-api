d38aa15099a7884fed897f44caa12e50e93ddd9f
Wahidallah Halim <161017694+Wahid-Halim@users.noreply.github.com>
2026-01-08 19:30:37 +0430

    Fix update and delete route comments in user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,9 @@ const userRouter = Router();
 userRouter.get("/", (req, res) => res.json({ body: "GET all users" }));
 userRouter.post("/", (req, res) => res.send("POST a user"));
 userRouter.get("/:id", (req, res) => res.send("GET a single user"));
+// updateing 
 userRouter.put("/:id", (req, res) => res.send("PATCH all users"));
+// delete route 
 userRouter.delete("/:id", (req, res) => res.send("DELETE a user"));
 
 export default userRouter;
